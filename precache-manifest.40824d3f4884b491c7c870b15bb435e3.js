18e5d5fecebb7e6971dc5d5df2babb3e29833ebc
Kaeldir <legal.mikael@gmail.com>
2020-09-07 12:47:15 +0200

    Updates

---
+++ precache-manifest.40824d3f4884b491c7c870b15bb435e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29352ac8eef421f053118728ab9ec4a",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "a3aee229eaa2de0fc2a3",
+    "url": "/dice-roller/static/css/main.c8dd2af5.chunk.css"
+  },
+  {
+    "revision": "606ee250ccdbb186772b",
+    "url": "/dice-roller/static/js/2.788802b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dice-roller/static/js/2.788802b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3aee229eaa2de0fc2a3",
+    "url": "/dice-roller/static/js/main.bebe40c0.chunk.js"
+  },
+  {
+    "revision": "1fef78c38f42099e7f04",
+    "url": "/dice-roller/static/js/runtime-main.f9aa7fc1.js"
+  }
+]);
